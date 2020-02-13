0082095cfb2ada39cd81327308aa1e49a23b7a64
auronel <informaticosasdaw@gmail.com>
2020-02-13 18:35:44 +0100

    Practica marcas concesionario

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Marca;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class MarcaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $paises=['España','Francia','Alemania','Italia','USA','Japon'];
+        $misPaises=$request->get('pais');
+        $marcas = Marca::orderBy('nombre')
+        ->pais($misPaises)
+        ->paginate(3);
+        return view('marcas.index', compact('marcas','paises','request'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $marcas = Marca::orderBy('nombre')->get();
+        return view('marcas.create', compact('marcas'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => ['required', 'unique:marcas,nombre'],
+        ]);
+
+        if ($request->has('logo')) {
+            $request->validate([
+                'logo' => ['image']
+            ]);
+            $file = $request->file('logo');
+            $nombre = 'marcas/' . time() . '_' . $file->getClientOriginalName();
+            Storage::disk('public')->put($nombre, \File::get($file));
+            $coche = Marca::create($request->all());
+            $coche->update(['logo' => "img/$nombre"]);
+        } else {
+            Marca::create($request->all());
+        }
+        return redirect()->route('marcas.index')->with("mensaje", "Marca guardada");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Marca $marca)
+    {
+        return view('marcas.detalle', compact('marca'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Marca $marca)
+    {
+        $marcas = Marca::orderBy('nombre')->get();
+        return view('marcas.edit', compact('marcas', 'marca'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Marca $marca)
+    {
+        $request->validate([
+            'nombre' => ['required', 'unique:marcas,nombre,' . $marca->id]
+        ]);
+
+        if ($request->has('logo')) {
+            $request->validate([
+                'logo' => ['image']
+            ]);
+            $file = $request->file('logo');
+            $nombre = 'marcas/' . time() . '_' . $file->getClientOriginalName();
+            Storage::disk('public')->put($nombre, \File::get($file));
+            if (basename($marca->logo) != 'default.jpg') {
+                unlink($marca->logo);
+            }
+            $marca->update($request->all());
+            $marca->update(['logo' => "img/$nombre"]);
+        } else {
+            $marca->update($request->all());
+        }
+        return redirect()->route('marcas.index')->with("mensaje", "Marca modificada");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Marca  $marca
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Marca $marca)
+    {
+        $logo = $marca->logo;
+        if (basename($logo) != "default.jpg") {
+            unlink($logo);
+        }
+
+        $marca->delete();
+        return redirect()->route('marcas.index')->with('mensaje', "Marca eliminada");
+    }
+}
---
+++ resources/views/marcas/index.blade.php
@@ -0,0 +1,63 @@
+@extends('plantillas.plantilla')
+@section('titulo')
+Marcas
+@endsection
+@section('cabecera')
+Marcas Disponibles
+@endsection
+@section('contenido')
+    @if($texto=Session::get('mensaje'))
+        <p class="alert alert-success my-3">{{$texto}}</p>
+    @endif
+    <div class="container">
+        <a href="{{route('marcas.create')}}" class="btn btn-success mb-3">Guardar marca</a>
+        <form name="search" method="get" action="{{route('marcas.index')}}" class="form-inline float-right">
+            <i class="fa fa-search fa-2x ml-2 mr-2" aria-hidden="true"></i>
+            <select name='pais' class='form-control mr-2' onchange="this.form.submit()">
+                <option value='%'>Todos</option>
+                    @foreach($paises as $pais)
+                        @if($pais==$request->pais)
+                            <option selected>{{$pais}}</option>
+                        @else
+                            <option >{{$pais}}</option>
+                        @endif
+                    @endforeach
+            </select>     
+        </form>
+        <table class="table table-striped table-dark mt-3">
+            <thead>
+                <tr class="text-center">
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Pais</th>
+                    <th scope="col">Logo</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($marcas as $marca)
+                <tr class="text-center">
+                    <th scope="row" class="align-middle">
+                        <a href="{{route('marcas.show', $marca)}}" class="btn btn-info">Detalles</a>
+                    </th>
+                    <td class="align-middle">{{$marca->nombre}}</td>
+                    <td class="align-middle">{{$marca->pais}}</td>
+                    <td>
+                        <img src="{{asset($marca->logo)}}" width="90px" height='90px' class="rounded-circle">
+                    </td>
+                    <td class="align-middle" style="white-space: nowrap">
+                        <form name="borrar" method='post' action='{{route('marcas.destroy', $marca)}}'>
+                            @csrf
+                            @method('DELETE')
+                            <a href='{{route('marcas.edit', $marca)}}' class="btn btn-warning">Editar</a>
+                            <button type='submit' class="btn btn-danger" onclick="return confirm('¿Borrar marca?')">
+                            Borrar</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{$marcas->appends(Request::except('page'))->links()}}
+    </div>
+  @endsection
